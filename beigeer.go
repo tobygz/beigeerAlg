1fef022966ccd87d0d268cdb37d737794b15c028
shuiyd <2503417117@qq.com>
2018-03-15 17:59:16 +0800

    Update beigeer.go

--- beigeer.go
+++ beigeer.go
@@ -1,92 +1,93 @@
 package main
 
 import (
-	"fmt"
+        "fmt"
 )
 
 func main() {
-	parseAry([]int{1, 2, 3, 4, 5, 6, 7})
-	//parseAry([]int{1, 2, 3, 4, 5, 6, 7, 8})
+        parseAry([]int{1, 2, 3, 4, 5, 6, 7})
+        //parseAry([]int{1, 2, 3, 4, 5, 6, 7, 8})
 }
 
 func revSlc(s []int) []int {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-	return s
+        for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+                s[i], s[j] = s[j], s[i]
+        }
+        return s
 }
 
 func parseAry(srcAry []int) [][][]int {
-	idx := 0
-	retSlice := make([][]int, 0)
-	aryLen := len(srcAry)
-	initLen := aryLen - 1
-	if len(srcAry)%2 != 0 {
-		srcAry = append(srcAry, 0)
-		initLen = aryLen
-	}
-	idx++
+        retSlice := make([][]int, 0)
+        aryLen := len(srcAry)
+        initLen := aryLen - 1
+        if len(srcAry)%2 != 0 {
+                srcAry = append(srcAry, 0)
+                initLen = aryLen
+        }
 
-	step := 1
-	if aryLen > 4 {
-		step = (aryLen-4)/2 + 1
-	}
+        step := 1
+        if aryLen > 4 {
+                step = (aryLen-4)/2 + 1
+        }
 
-	max := srcAry[len(srcAry)-1]
-	//shift
-	tmpAry := make([]int, len(srcAry))
-	copy(tmpAry, srcAry)
-	for n := 0; n < initLen; n++ {
+        max := srcAry[len(srcAry)-1]
+        //shift
+        tmpAry := make([]int, len(srcAry))
+        copy(tmpAry, srcAry)
+        for n := 0; n < initLen; n++ {
 
-		isMax := false
-		if tmpAry[0] == max {
-			isMax = true
-		}
+                isMax := false
+                if tmpAry[0] == max {
+                        isMax = true
+                }
 
-		rmIdx := len(tmpAry) - 1
-		if isMax {
-			rmIdx = 0
-		}
-		//remove empty
-		tmpAry = append(tmpAry[:rmIdx], tmpAry[rmIdx+1:]...)
+                rmIdx := len(tmpAry) - 1
+                if isMax {
+                        rmIdx = 0
+                }
+                //remove empty
+                tmpAry = append(tmpAry[:rmIdx], tmpAry[rmIdx+1:]...)
 
-		tmpAryLen := len(tmpAry)
-		stepLen := step
-		if isMax {
-			stepLen = step + 2
-		}
-		for i := 0; i < stepLen; i++ {
-			temp := tmpAry[tmpAryLen-1]
-			for j := tmpAryLen - 2; j >= 0; j-- {
-				tmpAry[j+1] = tmpAry[j]
-			}
-			tmpAry[0] = temp
-		}
+                tmpAryLen := len(tmpAry)
+                stepLen := step
+                if isMax {
+                        stepLen = step + 2
+                }
+                for i := 0; i < stepLen; i++ {
+                        temp := tmpAry[tmpAryLen-1]
+                        for j := tmpAryLen - 2; j >= 0; j-- {
+                                tmpAry[j+1] = tmpAry[j]
+                        }
+                        tmpAry[0] = temp
+                }
 
-		//bukong
-		if isMax {
-			tmpAry = append(tmpAry, max)
-		} else {
-			tmpAry = append([]int{max}, tmpAry[0:]...)
-		}
-		//fmt.Println(tmpAry)
-		dstAry := make([]int, len(srcAry))
-		copy(dstAry, tmpAry)
-		retSlice = append(retSlice, dstAry)
-	}
+                //bukong
+                if isMax {
+                        tmpAry = append(tmpAry, max)
+                } else {
+                        tmpAry = append([]int{max}, tmpAry[0:]...)
+                }
+                fmt.Println(tmpAry)
+                dstAry := make([]int, len(srcAry))
+                copy(dstAry, tmpAry)
+                retSlice = append(retSlice, dstAry)
+        }
+        fmt.Println("")
 
-	//get pairs
-	pairSlc := make([][][]int, 0)
-	for _, slc := range retSlice {
-		roundSlc := make([][]int, 0)
-		nowLen := len(slc) / 2
-		left := slc[:nowLen]
-		right := revSlc(slc[nowLen:])
-		for eidx, _ := range left {
-			roundSlc = append(roundSlc, []int{left[eidx], right[eidx]})
-		}
-		pairSlc = append(pairSlc, roundSlc)
-	}
+        //get pairs
+        pairSlc := make([][][]int, 0)
+        for rid, slc := range retSlice {
+                roundSlc := make([][]int, 0)
+                nowLen := len(slc) / 2
+                left := slc[:nowLen]
+                right := revSlc(slc[nowLen:])
+                fmt.Println("\r\n\r\nround ", rid)
+                for eidx, _ := range left {
+                        fmt.Println(left[eidx], " vs ", right[eidx])
+                        roundSlc = append(roundSlc, []int{left[eidx], right[eidx]})
+                }
+                pairSlc = append(pairSlc, roundSlc)
+        }
 
-	return pairSlc
+        return pairSlc
 }
